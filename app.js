00899201e0bf411ae05c141302ee6be2991d6a7c
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-27 16:58:29 +0100

    Added extra packages

--- app.js
+++ app.js
@@ -1,44 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 const prisma = new PrismaClient();
 
-async function main() {
-	// Run only once
-	// await prisma.user.create({
-	// 	data: {
-	// 		name: "Alice",
-	// 		email: "alice@prisma.io",
-	// 		posts: {
-	// 			create: { title: "Hello World" },
-	// 		},
-	// 		profile: {
-	// 			create: { "bio": "I like turtles" },
-	// 		},
-	// 	},
-	// });
-
-	// const allUsers = await prisma.user.findMany({
-	// 	include: {
-	// 		posts: true,
-	// 		profile: true,
-	// 	}
-	// });
-	// console.dir(allUsers, { depth: null });
-	const post = await prisma.post.update({
-		where: { id: 1 },
-		data: { published: true },
-	});
-	console.log(post);
-}
-
-main()
-	.then(async () => {
-		await prisma.$disconnect();
-	})
-	.catch(async(e) => {
-		console.error(e);
-		await prisma.$disconnect();
-		process.exit(1);
-	});
+app.listen(process.env.PORT || 3000, () => console.log("App listening in port 3000"));
