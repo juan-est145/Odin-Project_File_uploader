f97947a98e7ac709867bf1b0c3d6100b51c79e6d
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-27 17:10:35 +0100

    Made alias for path directories

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 require("dotenv").config();
+const { testCont } = require("#controllers/test.js")
+
 
 const app = express();
 const prisma = new PrismaClient();
+
+app.use("/", testCont);
 
 app.listen(process.env.PORT || 3000, () => console.log("App listening in port 3000"));
---
+++ controllers/test.js
@@ -0,0 +1,7 @@
+function testCont(req, res) {
+	res.send("Hola caracola");
+}
+
+module.exports = {
+	testCont
+}
