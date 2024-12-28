30b7b325378337f2f2cbb100c13417198764e304
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-28 19:12:39 +0100

    Created basic views for login and register

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,11 @@ function getIndex(req, res) {
 	res.render("index");
 }
 
+function getSignUp(req, res) {
+	res.render("signUp");
+}
+
 module.exports = {
 	getIndex,
+	getSignUp,
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,5 +4,6 @@ const controllers = require("#controllers/indexController.js");
 const indexRouter = Router();
 
 indexRouter.get("/", controllers.getIndex);
+indexRouter.get("/sign-up", controllers.getSignUp);
 
 module.exports = indexRouter;
