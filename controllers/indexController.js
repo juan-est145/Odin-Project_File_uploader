55e22c86b328486418dc27f7ad47056237465ac6
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-28 19:42:14 +0100

    Created basic validation for post auth

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,9 @@
+const { body, validationResult } = require("express-validator");
+const { passportAuth } = require("#auth/passport.js");
+const bcrypt = require("bcryptjs");
+
+const minPswdLength = 8;
+
 function getIndex(req, res) {
 	res.render("index");
 }
@@ -6,7 +12,61 @@ function getSignUp(req, res) {
 	res.render("signUp");
 }
 
+const postSignIn = [
+	[
+		body("username").trim()
+			.isLength({ min: 1, max: 255 }).withMessage("Invalid username or password"),
+		body("password").trim()
+			.isLength({ min: minPswdLength, max: 255 }).withMessage("Invalid username or password"),
+		body("repeatPassword").trim()
+			.custom((value, { req }) => {
+				if (value !== req.body.password)
+					throw new Error("Passwords do not match");
+				return (true);
+			}),
+	],
+	async function signUp(req, res, next) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			//Might need to use later req.flash
+			return res.status(400).redirect("/sign-up")
+		}
+		try {
+			const hash = await bcrypt.hash(req.body.password, 10);
+			// Implement sign up function
+			return passportAuth(req, res, next);
+		} catch (error) {
+			console.error(error);
+			//Implement sign up error handle
+		}
+	}
+];
+
+const postLogIn = [
+	[
+		body("username").trim()
+			.isLength({ min: 1, max: 255 }).withMessage("Invalid username or password"),
+		body("password").trim()
+			.isLength({ min: minPswdLength, max: 255 }).withMessage("Invalid username or password"),
+	],
+	async function logIn(req, res, next) {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				// Might need to use later req.flash
+				return res.status(400).redirect("/");
+			}
+			return passportAuth(req, res, next);
+		} catch (error) {
+			console.error(error);
+			next(error);
+		}
+	}
+];
+
 module.exports = {
 	getIndex,
 	getSignUp,
+	postSignIn,
+	postLogIn,
 };
