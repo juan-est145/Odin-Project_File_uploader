b4110d44aef817584daa97e98aca586a622530dd
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-29 10:48:59 +0100

    Added public folders and added ejs partials

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const storageRouter = require("#routes/storage.js");
 const app = express();
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(sessionConfig);
 app.use(passportConf);
