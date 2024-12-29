0f6f86331c448bb6d8001c64849872184dbc0093
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-12-29 12:57:06 +0100

    Non users cannot access the storage route

--- routes/storage.js
+++ routes/storage.js
@@ -1,6 +1,15 @@
 const { Router } = require("express");
 
 const storageRouter = Router();
+
+storageRouter.use((req, res, next) => {
+	if (!req.isAuthenticated())
+		return res.status(401).send("Not for now");
+	next();
+	//Later implement a view for not logged users
+});
+
+
 storageRouter.get("/", (req, res, next) => {
 	if (!req.isAuthenticated())
 		return res.send("You are not logged in");
